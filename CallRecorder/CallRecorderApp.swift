33c357488567a70ff11fed1331f34e2375e555a3
IOENKIS <90310655+IOENKIS@users.noreply.github.com>
2024-07-15 12:54:25 +0300

    Initial Commit

---
+++ CallRecorder/CallRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CallRecorderApp.swift
+//  CallRecorder
+//
+//  Created by IVANKIS on 15.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CallRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
